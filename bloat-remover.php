053aa7caf3f27318020d7e64c9760602e27ac70e
Rao Abid <rao@booskills.com>
2021-02-11 20:48:34 +0500

    added more functions, after discarding idea of separate plugin ByMolvi-ForMolvi

--- bloat-remover.php
+++ bloat-remover.php
@@ -8,33 +8,17 @@ add_action( 'wp_enqueue_scripts', function () {
 
 	if ( in_array( $post->ID, $pageID ) ) :
 
-		//common woocommerce styles and scrips
+		// wp_dequeue_style('styleID');
+		// wp_deregister_style('styleID');
+		// wp_dequeue_script('scriptID');
+		// wp_deregister_script('scriptID');
 
-		// wp_dequeue_style('wp-block-library');
-		// wp_dequeue_style('wc-block-style');
-		// wp_dequeue_style('wc-block-vendors-style');
-		// wp_dequeue_style('woocommerce-layout');
-		// wp_dequeue_style('woocommerce-smallscreen');
-		// wp_dequeue_style('woocommerce-general');
-		// wp_dequeue_style('photoswipe');
-		// wp_dequeue_style('photoswipe-default-skin');
-		// wp_dequeue_style('woocommerce-inline-inline');
-
-		// wp_dequeue_script('wc-add-to-cart');
-		// wp_dequeue_script('jquery-blockui');
-		// wp_dequeue_script('woocommerce');
-		// wp_dequeue_script('zoom');
-
-		// wp_dequeue_script('wc-cart-fragments');
-		// wp_dequeue_script('flexslider');
-		// wp_dequeue_script('photoswipe-ui-default');
-		// wp_dequeue_script('wc-single-product');
 	endif;
 }, 999 );
 
 
 /**
- * Remove Bloat everywhere except
+ * Remove Bloat everywhere EXCEPT
  */
 add_action( 'wp_enqueue_scripts', function () {
 	global $post;
@@ -42,26 +26,43 @@ add_action( 'wp_enqueue_scripts', function () {
 
 	if ( ! in_array( $post->ID, $pageID ) ) :
 
-		//common woocommerce styles and scrips
+		// wp_dequeue_style('styleID');
+		// wp_deregister_style('styleID');
+		// wp_dequeue_script('scriptID');
+		// wp_deregister_script('scriptID');
 
-		// wp_dequeue_style('wp-block-library');
-		// wp_dequeue_style('wc-block-style');
-		// wp_dequeue_style('wc-block-vendors-style');
-		// wp_dequeue_style('woocommerce-layout');
-		// wp_dequeue_style('woocommerce-smallscreen');
-		// wp_dequeue_style('woocommerce-general');
-		// wp_dequeue_style('photoswipe');
-		// wp_dequeue_style('photoswipe-default-skin');
-		// wp_dequeue_style('woocommerce-inline-inline');
+	endif;
+}, 999 );
 
-		// wp_dequeue_script('wc-add-to-cart');
-		// wp_dequeue_script('jquery-blockui');
-		// wp_dequeue_script('woocommerce');
-		// wp_dequeue_script('zoom');
 
-		// wp_dequeue_script('wc-cart-fragments');
-		// wp_dequeue_script('flexslider');
-		// wp_dequeue_script('photoswipe-ui-default');
-		// wp_dequeue_script('wc-single-product');
+/**
+ * Remove Bloat Specific Post Type
+ */
+add_action( 'wp_enqueue_scripts', function () {
+
+	$post_types = array( 'post', 'product', 'page' );
+
+	if ( in_array( get_post_type( get_the_ID() ), $post_types, true ) ) :
+
+		// wp_dequeue_style('styleID');
+		// wp_deregister_style('styleID');
+		// wp_dequeue_script('scriptID');
+		// wp_deregister_script('scriptID');
+
+	endif;
+}, 999 );
+
+/**
+ * Remove Bloat All archive
+ */
+add_action( 'wp_enqueue_scripts', function () {
+
+	if ( is_archive() ) :
+
+		// wp_dequeue_style('styleID');
+		// wp_deregister_style('styleID');
+		// wp_dequeue_script('scriptID');
+		// wp_deregister_script('scriptID');
+
 	endif;
 }, 999 );
